fac53fe53f41d7bc4726ef54a17a2cb694c10787
FSCB21 <freddystibencb@gmail.com>
2021-12-04 16:57:26 -0500

    ajustes para heroku// BD

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
     database: {
-        username: "root",
-        password: "",
-        database: "bd_sacris",
-        host: "localhost"
+        username: "b005d3d8106f32",
+        password: "10b5ec21",
+        database: "heroku_d32136a5573b13d",
+        host: "us-cdbr-east-04.cleardb.com"
     }
 
 }
